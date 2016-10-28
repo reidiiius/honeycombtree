416db96e2da43d8402ce35d8478ddfd3c42dc47f
Reid Netterville III <reidiiius@gmail.com>
2016-10-28 03:48:52 -0500

    161028.0

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# honeycombtree
+# Honeycombtree
 Apiculture
+
+---
+
+### Download
+
+    git clone https://github.com/reidiiius/honeycombtree.git
+
+---
+
+### Usage
+command line
+
+    cargo build
+
+    cargo run | less
+
+---
+
+### Distribution
+MIT License
+
---
+++ src/main.rs
@@ -0,0 +1,104 @@
+use std::collections::BTreeMap;
+
+fn main() {
+    let mut zz = BTreeMap::new();
+     zz.insert("j2","HgHg PuFe ____ ____ CuNp PbAu ____ AuPb ____ AgUr ____ FePu ");
+     zz.insert("j3","HgSn ____ SnHg UrFe ____ PbAg ____ AuAu ____ AgPb ____ FeUr ");
+     zz.insert("j5","PbCu ____ AuSn ____ AgHg TiFe FeTi ____ ____ SnAu ____ CuPb ");
+     zz.insert("j6","HgAu ____ SnPb ____ CuUr PbSn ____ AuHg NpFe ____ ____ FeNp ");
+     zz.insert("k1","____ FeUr HgSn ____ SnHg UrFe ____ PbAg ____ AuAu ____ AgPb ");
+     zz.insert("k2","NpCu ____ ____ FePu HgHg PuFe ____ UrAg ____ PbAu ____ AuPb ");
+     zz.insert("k5","UrCu ____ PbSn ____ AuHg NpFe ____ ____ FeNp HgAu ____ SnPb ");
+     zz.insert("k6","HgAg ____ SnAu ____ CuPb PbCu ____ AuSn ____ ____ TiFe FeTi ");
+     zz.insert("n0","HgCu ____ SnSn ____ CuHg PbFe ____ AuAg ____ AgAu ____ FePb ");
+    zz.insert("j17","____ ____ SnAu ____ CuPb PbCu ____ AuSn ____ AgHg TiFe FeTi ");
+    zz.insert("j23","HgHg PuFe ____ UrAg ____ PbAu ____ AuPb ____ AgUr ____ FePu ");
+    zz.insert("j25","FeCu HgMn ____ ____ MnHg CuFe PbTi ____ ____ NpAu ____ TiPb ");
+    zz.insert("j26","HgHg PuFe ____ ____ CuNp PbAu ____ AuPb NpCu ____ ____ FePu ");
+    zz.insert("j36","HgAu ____ SnPb UrCu ____ PbSn ____ AuHg NpFe ____ ____ FeNp ");
+    zz.insert("j56","UrCu ____ PbSn ____ AuHg NpFe AgTi ____ FeNp ____ ____ SnPb ");
+    zz.insert("k12","____ AgUr ____ FePu HgHg PuFe ____ UrAg ____ PbAu ____ AuPb ");
+    zz.insert("k15","____ CuUr PbSn ____ AuHg NpFe ____ ____ FeNp HgAu ____ SnPb ");
+    zz.insert("k25","NpCu ____ ____ FePu HgHg PuFe ____ ____ CuNp PbAu ____ AuPb ");
+    zz.insert("k26","HgMn ____ ____ MnHg CuFe PbTi ____ AuNp ____ ____ TiPb FeCu ");
+    zz.insert("k34","PbCu ____ AuSn ____ ____ TiFe FeTi HgAg ____ SnAu ____ CuPb ");
+    zz.insert("k56","HgAu ____ SnPb ____ CuUr PbSn ____ ____ NpFe ____ TiAg FeNp ");
+   zz.insert("j236","HgHg PuFe ____ UrAg ____ PbAu ____ AuPb NpCu ____ ____ FePu ");
+   zz.insert("j256","FeCu HgMn ____ ____ MnHg CuFe PbTi ____ AuNp ____ ____ TiPb ");
+   zz.insert("j2k5","FeCu HgMn ____ ____ MnHg CuFe ____ ____ AuNp NpAu ____ TiPb ");
+   zz.insert("j2k6","HgHg PuFe ____ ____ CuNp PbAu ____ AuPb ____ ____ TiSn FePu ");
+   zz.insert("j2y3","HgHg PuFe SnTi ____ ____ PbAu ____ AuPb ____ AgUr ____ FePu ");
+   zz.insert("j3k5","NpCu ____ TiSn FePu ____ PuFe ____ ____ CuNp PbAu ____ AuPb ");
+   zz.insert("j3k6","HgTi ____ SnNp UrAu ____ PbPb ____ AuUr ____ ____ TiHg FeFe ");
+   zz.insert("j5y6","PbCu ____ AuSn ____ AgHg TiFe FeTi HgAg ____ ____ ____ CuPb ");
+   zz.insert("k125","____ AgUr ____ FePu HgHg PuFe ____ ____ CuNp PbAu ____ AuPb ");
+   zz.insert("k1j5","____ AuUr NpSn ____ TiHg FeFe HgTi ____ ____ UrAu ____ PbPb ");
+   zz.insert("k1j6","____ PuFe SnTi ____ CuNp PbAu ____ AuPb NpCu ____ ____ FePu ");
+   zz.insert("k256","HgMn ____ ____ MnHg CuFe PbTi ____ ____ NpAu ____ TiPb FeCu ");
+   zz.insert("k2j5","NpCu ____ ____ FePu HgHg PuFe SnTi ____ ____ PbAu ____ AuPb ");
+   zz.insert("k2j6","HgMn ____ ____ MnHg CuFe PbTi ____ AuNp NpAu ____ ____ FeCu ");
+   zz.insert("k2x1","____ ____ TiSn FePu HgHg PuFe ____ UrAg ____ PbAu ____ AuPb ");
+   zz.insert("k6x5","HgAg ____ SnAu ____ CuPb PbCu ____ ____ ____ AgHg TiFe FeTi ");
+   zz.insert("n167","NpCu ____ ____ FePu ____ PuFe SnTi ____ CuNp PbAu ____ AuPb ");
+   zz.insert("n345","____ PuFe ____ ____ CuNp PbAu ____ AuPb NpCu ____ TiSn FePu ");
+   zz.insert("n5y2","HgMn ____ ____ MnHg CuFe ____ ____ AuNp NpAu ____ TiPb FeCu ");
+   zz.insert("n6x2","FeCu HgMn ____ ____ MnHg CuFe PbTi ____ AuNp NpAu ____ ____ ");
+  zz.insert("j17k2","____ ____ ____ MnFe CuTi PbAg ____ AuAu ____ AgPb TiCu FeMn ");
+  zz.insert("j17y2","HgAg ____ ____ ____ CuPb PbCu ____ AuSn ____ AgHg TiFe FeTi ");
+  zz.insert("j23k6","HgHg PuFe ____ UrAg ____ PbAu ____ AuPb ____ ____ TiSn FePu ");
+  zz.insert("j25y6","TiCu FeMn ____ ____ SnHg MnFe CuTi PbAg ____ ____ ____ AgPb ");
+  zz.insert("j26y3","HgHg PuFe SnTi ____ ____ PbAu ____ AuPb NpCu ____ ____ FePu ");
+  zz.insert("j2k34","TiCu FeMn ____ ____ ____ MnFe CuTi PbAg ____ AuAu ____ AgPb ");
+  zz.insert("j2k56","HgHg PuFe ____ ____ CuNp PbAu ____ ____ NpCu ____ TiSn FePu ");
+  zz.insert("j34k6","HgSn ____ SnHg MnFe CuTi ____ ____ AuAu ____ ____ TiCu FeMn ");
+  zz.insert("j56y7","UrCu ____ PbSn ____ AuHg NpFe AgTi ____ FeNp HgAu ____ ____ ");
+  zz.insert("k12j5","____ AgUr ____ FePu HgHg PuFe SnTi ____ ____ PbAu ____ AuPb ");
+  zz.insert("k17j5","TiCu FeMn HgSn ____ SnHg MnFe CuTi ____ ____ AuAu ____ ____ ");
+  zz.insert("k25x1","____ ____ TiSn FePu HgHg PuFe ____ ____ CuNp PbAu ____ AuPb ");
+  zz.insert("k26x5","HgSn ____ ____ MnFe CuTi PbAg ____ ____ ____ AgPb TiCu FeMn ");
+  zz.insert("k2j56","NpCu ____ ____ FePu HgHg PuFe SnTi ____ CuNp ____ ____ AuPb ");
+  zz.insert("k34x2","PbCu ____ ____ ____ AgHg TiFe FeTi HgAg ____ SnAu ____ CuPb ");
+  zz.insert("k56x4","HgAu ____ SnPb ____ CuUr ____ ____ AuHg NpFe ____ TiAg FeNp ");
+  zz.insert("n25x6","TiCu FeMn HgSn ____ ____ MnFe CuTi PbAg ____ ____ ____ AgPb ");
+  zz.insert("n26y5","____ ____ SnHg MnFe CuTi PbAg ____ ____ ____ AgPb TiCu FeMn ");
+  zz.insert("n45y2","HgTi ____ ____ UrAu ____ PbPb ____ AuUr NpSn ____ TiHg FeFe ");
+  zz.insert("n67x2","____ AuUr ____ ____ TiHg FeFe HgTi ____ SnNp UrAu ____ PbPb ");
+ zz.insert("j136y7","____ ____ SnPb UrCu ____ PbSn ____ AuHg NpFe AgTi ____ FeNp ");
+ zz.insert("j167y2","HgAu ____ ____ ____ CuUr PbSn ____ AuHg NpFe ____ TiAg FeNp ");
+ zz.insert("j246y3","HgHg PuFe SnTi ____ CuNp ____ ____ AuPb NpCu ____ ____ FePu ");
+ zz.insert("j26y34","HgHg PuFe SnTi UrAg ____ ____ ____ AuPb NpCu ____ ____ FePu ");
+ zz.insert("j2k6x5","HgHg PuFe ____ ____ CuNp PbAu ____ ____ ____ AgUr TiSn FePu ");
+ zz.insert("j2k6y3","HgHg PuFe SnTi ____ ____ PbAu ____ AuPb ____ ____ TiSn FePu ");
+ zz.insert("j346y5","NpCu ____ TiSn FePu HgHg PuFe ____ ____ CuNp ____ ____ AuPb ");
+ zz.insert("j3k5x4","HgAu ____ SnPb UrCu ____ ____ ____ AuHg NpFe AgTi ____ FeNp ");
+ zz.insert("k135x4","____ CuUr PbSn ____ ____ NpFe ____ TiAg FeNp HgAu ____ SnPb ");
+ zz.insert("k157x6","HgHg PuFe SnTi ____ CuNp PbAu ____ ____ NpCu ____ ____ FePu ");
+ zz.insert("k1j6y7","____ CuUr PbSn ____ AuHg NpFe ____ TiAg FeNp HgAu ____ ____ ");
+ zz.insert("k257x1","NpCu ____ TiSn FePu HgHg PuFe ____ ____ CuNp PbAu ____ ____ ");
+ zz.insert("k25x17","____ AgUr TiSn FePu HgHg PuFe ____ ____ CuNp PbAu ____ ____ ");
+ zz.insert("k2j5x1","____ ____ TiSn FePu HgHg PuFe SnTi ____ ____ PbAu ____ AuPb ");
+ zz.insert("k2j5y6","NpCu ____ ____ FePu HgHg PuFe SnTi UrAg ____ ____ ____ AuPb ");
+ zz.insert("k345x2","UrCu ____ ____ ____ AuHg NpFe AgTi ____ FeNp HgAu ____ SnPb ");
+ zz.insert("n167x4","HgAu ____ ____ UrCu ____ PbSn ____ AuHg NpFe AgTi ____ FeNp ");
+ zz.insert("n345y7","____ CuUr ____ ____ AuHg NpFe ____ TiAg FeNp HgAu ____ SnPb ");
+zz.insert("j2k56x4","HgHg PuFe ____ ____ CuNp ____ ____ AuPb NpCu ____ TiSn FePu ");
+zz.insert("j3k56x4","HgTi ____ SnNp UrAu ____ ____ ____ AuUr NpSn ____ TiHg FeFe ");
+zz.insert("k1j56y7","____ AuUr NpSn ____ TiHg FeFe HgTi ____ SnNp UrAu ____ ____ ");
+zz.insert("k2j56y7","NpCu ____ ____ FePu HgHg PuFe SnTi ____ CuNp PbAu ____ ____ ");
+
+    println!("");
+    for (k, v) in zz {
+        println!("");
+        println!("{}", "\t".to_string() + k);
+        println!("{}", "\t".to_string() + (&v[25..60]) + &v[00..25]); // Fn
+        println!("{}", "\t".to_string() + (&v[00..60]) + &v[00..00]); // Cn
+        println!("{}", "\t".to_string() + (&v[35..60]) + &v[00..35]); // Gn
+        println!("{}", "\t".to_string() + (&v[10..60]) + &v[00..10]); // Dn
+        println!("{}", "\t".to_string() + (&v[45..60]) + &v[00..45]); // An
+        println!("{}", "\t".to_string() + (&v[20..60]) + &v[00..20]); // En
+        println!("{}", "\t".to_string() + (&v[55..60]) + &v[00..55]); // Bn
+        println!("");
+    }
+    println!("");
+}
+
