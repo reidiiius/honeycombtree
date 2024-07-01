a92267dce0e0c8323e6e6d29e8161ae837fa9975
Reid Netterville III <reidiiius@gmail.com>
2024-07-01 16:56:21 -0500

    1719870981

--- src/main.rs
+++ src/main.rs
@@ -1,108 +1,146 @@
 use std::collections::BTreeMap;
 
 fn main() {
+    let mut hive = BTreeMap::new();
+    hive.insert("i0", "__ __ __ __ __ __ __ __ __ __ __ __ ");
+    hive.insert("j136l7", "__ __ tw xr __ wt __ uv yq so __ qy ");
+    hive.insert("j167l2", "vu __ __ __ rx wt __ uv yq __ os qy ");
+    hive.insert("j17l2", "vs __ __ __ rw wr __ ut __ sv oq qo ");
+    hive.insert("j17", "__ __ tu __ rw wr __ ut __ sv oq qo ");
+    hive.insert("j17zy", "__ zy tu __ rw wr __ ut __ sv oq qo ");
+    hive.insert("j236ot", "vv zq __ xs __ wu __ uw yr __ ot qz ");
+    hive.insert("j236sp", "vv zq __ xs __ wu __ uw yr sp __ qz ");
+    hive.insert("j236sq", "vy zu __ xw __ wx __ uz yv sq __ qs ");
+    hive.insert("j236", "vv zq __ xs __ wu __ uw yr __ __ qz ");
+    hive.insert("j23k6", "vv zq __ xs __ wu __ uw __ __ ot qz ");
+    hive.insert("j23ot", "vv zq __ xs __ wu __ uw __ sx ot qz ");
+    hive.insert("j23to", "vv zq to xs __ wu __ uw __ sx __ qz ");
+    hive.insert("j23", "vv zq __ xs __ wu __ uw __ sx __ qz ");
+    hive.insert("j246l3", "vv zq to __ ry __ __ uw yr __ __ qz ");
+    hive.insert("j256", "qr vp __ __ pv rq wo __ uy __ __ ow ");
+    hive.insert("j25l6", "or qp __ __ tv pq ro ws __ __ __ sw ");
+    hive.insert("j25", "qr vp __ __ pv rq wo __ __ yu __ ow ");
+    hive.insert("j26l34", "vv zq to xs __ __ __ uw yr __ __ qz ");
+    hive.insert("j26l3", "vv zq to __ __ wu __ uw yr __ __ qz ");
+    hive.insert("j26ot", "vv zq __ __ ry wu __ uw yr __ ot qz ");
+    hive.insert("j26psp", "vv zq __ ps ry wu __ uw yr sp __ qz ");
+    hive.insert("j26ps", "vv zq __ ps ry wu __ uw yr __ __ qz ");
+    hive.insert("j26sp", "vv zq __ __ ry wu __ uw yr sp __ qz ");
+    hive.insert("j26to", "vv zq to __ ry wu __ uw yr __ __ qz ");
+    hive.insert("j26", "vv zq __ __ ry wu __ uw yr __ __ qz ");
+    hive.insert("j2k34", "or qp __ __ __ pq ro ws __ uu __ sw ");
+    hive.insert("j2k56m4", "vv zq __ __ ry __ __ uw yr __ ot qz ");
+    hive.insert("j2k56", "vv zq __ __ ry wu __ __ yr __ ot qz ");
+    hive.insert("j2k5", "qr vp __ __ pv rq __ __ uy yu __ ow ");
+    hive.insert("j2k6l3", "vv zq to __ __ wu __ uw __ __ ot qz ");
+    hive.insert("j2k6m5", "vv zq __ __ ry wu __ __ __ sx ot qz ");
+    hive.insert("j2k6", "vv zq __ __ ry wu __ uw __ __ ot qz ");
+    hive.insert("j2l3", "vv zq to __ __ wu __ uw __ sx __ qz ");
+    hive.insert("j2ps", "vv zq __ ps ry wu __ uw __ sx __ qz ");
+    hive.insert("j2to", "vv zq to __ ry wu __ uw __ sx __ qz ");
+    hive.insert("j2", "vv zq __ __ ry wu __ uw __ sx __ qz ");
+    hive.insert("j346l5", "yr __ ot qz vv zq __ __ ry __ __ uw ");
+    hive.insert("j346ow", "vp __ tz pv rq __ __ uy yu __ ow qr ");
+    hive.insert("j34k6", "vt __ tv pq ro __ __ uu __ __ or qp ");
+    hive.insert("j34or", "vt __ tv pq ro __ __ uu __ sw or qx ");
+    hive.insert("j36so", "vu __ tw xr __ wt __ uv yq so __ qy ");
+    hive.insert("j36", "vu __ tw xr __ wt __ uv yq __ __ qy ");
+    hive.insert("j3k16zs", "__ zs ty xu __ ww __ ux yt __ ov qq ");
+    hive.insert("j3k56m4", "vo __ ty xu __ __ __ ux yt __ ov qq ");
+    hive.insert("j3k5m4", "vu __ tw xr __ __ __ uv yq so __ qy ");
+    hive.insert("j3k5to", "yr __ ot qz __ zq to __ ry wu __ uw ");
+    hive.insert("j3k5", "yr __ ot qz __ zq __ __ ry wu __ uw ");
+    hive.insert("j3k6", "vo __ ty xu __ ww __ ux __ __ ov qq ");
+    hive.insert("j3k6yt", "vo __ ty xu __ ww __ ux yt __ ov qq ");
+    hive.insert("j3or", "vt __ tv xq __ ws __ uu __ sw or qp ");
+    hive.insert("j3ror", "vt __ tv xq ro ws __ uu __ sw or qp ");
+    hive.insert("j3ro", "vt __ tv pq ro ws __ uu __ sw __ qx ");
+    hive.insert("j3", "vt __ tv xq __ ws __ uu __ sw __ qx ");
+    hive.insert("j3zz", "vt zz tv xq __ ws __ uu __ sw __ qx ");
+    hive.insert("j56l7", "xr __ wt __ uv yq so __ qy vu __ __ ");
+    hive.insert("j56", "xr __ wt __ uv yq so __ qy __ __ tw ");
+    hive.insert("j5l6", "wr __ ut __ sv oq qo vs __ __ __ rw ");
+    hive.insert("j5", "wr __ ut __ sv oq qo __ __ tu __ rw ");
+    hive.insert("j6os", "vu __ tw __ rx wt __ uv yq __ os qy ");
+    hive.insert("j6pros", "vu __ tw pr rp wt __ uv yq __ os qy ");
+    hive.insert("j6prso", "vu __ tw pr rp wt __ uv yq so __ qy ");
+    hive.insert("j6pr", "vu __ tw pr rp wt __ uv yq __ __ qy ");
+    hive.insert("j6so", "vu __ tw __ rp wt __ uv yq so __ qy ");
+    hive.insert("j6", "vu __ tw __ rx wt __ uv yq __ __ qy ");
+    hive.insert("k125", "__ sx __ qz vv zq __ __ ry wu __ uw ");
+    hive.insert("k12j5", "__ sx __ qz vv zq to __ __ wu __ uw ");
+    hive.insert("k12", "__ sx __ qz vv zq __ xs __ wu __ uw ");
+    hive.insert("k135m4", "__ rx wt __ __ yq __ os qy vu __ tw ");
+    hive.insert("k157m6", "vv zq to __ ry wu __ __ yr __ __ qz ");
+    hive.insert("k15", "__ rx wt __ uv yq __ __ qy vu __ tw ");
+    hive.insert("k17j5", "or qp vt __ tv pq ro __ __ uu __ __ ");
+    hive.insert("k1j56l7", "__ ux yt __ ov qq vo __ ty xu __ __ ");
+    hive.insert("k1j5", "__ ux yt __ ov qq vo __ __ xu __ ww ");
+    hive.insert("k1j6l7", "__ rx wt __ uv yq __ os qy vu __ __ ");
+    hive.insert("k1j6ot", "__ zq to __ ry wu __ uw yr __ ot qz ");
+    hive.insert("k1j6", "__ zq to __ ry wu __ uw yr __ __ qz ");
+    hive.insert("k1", "__ qx vt __ tv xq __ ws __ uu __ sw ");
+    hive.insert("k256", "vp __ __ pv rq wo __ __ yu __ ow qr ");
+    hive.insert("k257m1", "yr __ ot qz vv zq __ __ ry wu __ __ ");
+    hive.insert("k25m17", "__ sx ot qz vv zq __ __ ry wu __ __ ");
+    hive.insert("k25m1", "__ __ ot qz vv zq __ __ ry wu __ uw ");
+    hive.insert("k25", "yr __ __ qz vv zq __ __ ry wu __ uw ");
+    hive.insert("k26m5", "vt __ __ pq ro ws __ __ __ sw or qp ");
+    hive.insert("k26sw", "vt __ __ pq ro ws __ uu __ sw or qp ");
+    hive.insert("k26tv", "vt __ tv pq ro ws __ uu __ __ or qx ");
+    hive.insert("k26", "vp __ __ pv rq wo __ uy __ __ ow qr ");
+    hive.insert("k26yu", "vp __ __ pv rq wo __ uy yu __ ow qr ");
+    hive.insert("k2j17", "__ __ __ pq ro ws __ uu __ sw or qp ");
+    hive.insert("k2j17tv", "__ __ tv pq ro ws __ uu __ sw or qp ");
+    hive.insert("k2j56l7", "yr __ __ qz vv zq to __ ry wu __ __ ");
+    hive.insert("k2j56", "yr __ __ qz vv zq to __ ry __ __ uw ");
+    hive.insert("k2j5l6", "yr __ __ qz vv zq to xs __ __ __ uw ");
+    hive.insert("k2j5m1", "__ __ ot qz vv zq to __ __ wu __ uw ");
+    hive.insert("k2j5", "yr __ __ qz vv zq to __ __ wu __ uw ");
+    hive.insert("k2j6tz", "vp __ tz pv rq wo __ uy yu __ __ qr ");
+    hive.insert("k2j6", "vp __ __ pv rq wo __ uy yu __ __ qr ");
+    hive.insert("k2j6zt", "vp zt __ pv rq wo __ uy yu __ __ qr ");
+    hive.insert("k2m1", "__ __ ot qz vv zq __ xs __ wu __ uw ");
+    hive.insert("k2", "yr __ __ qz vv zq __ xs __ wu __ uw ");
+    hive.insert("k345m2", "xr __ __ __ uv yq so __ qy vu __ tw ");
+    hive.insert("k34m2", "wr __ __ __ sv oq qo vs __ tu __ rw ");
+    hive.insert("k34", "wr __ ut __ __ oq qo vs __ tu __ rw ");
+    hive.insert("k56m4", "vu __ tw __ rx __ __ uv yq __ os qy ");
+    hive.insert("k56", "vu __ tw __ rx wt __ __ yq __ os qy ");
+    hive.insert("k5", "xr __ wt __ uv yq __ __ qy vu __ tw ");
+    hive.insert("k6m5", "vs __ tu __ rw wr __ __ __ sv oq qo ");
+    hive.insert("k6sv", "vs __ tu __ rw wr __ ut __ sv oq qo ");
+    hive.insert("k6", "vs __ tu __ rw wr __ ut __ __ oq qo ");
+    hive.insert("n0pz", "vr __ tt pz rv wq __ us __ su __ qw ");
+    hive.insert("n0pzyy", "vr __ tt pz rv wq __ us yy su __ qw ");
+    hive.insert("n0", "vr __ tt __ rv wq __ us __ su __ qw ");
+    hive.insert("n0yy", "vr __ tt __ rv wq __ us yy su __ qw ");
+    hive.insert("n0zp", "vr zp tt __ rv wq __ us __ su __ qw ");
+    hive.insert("n0zpyy", "vr zp tt __ rv wq __ us yy su __ qw ");
+    hive.insert("n167m4", "vu __ __ xr __ wt __ uv yq so __ qy ");
+    hive.insert("n167", "yr __ __ qz __ zq to __ ry wu __ uw ");
+    hive.insert("n25m6", "or qp vt __ __ pq ro ws __ __ __ sw ");
+    hive.insert("n26l5", "__ __ tv pq ro ws __ __ __ sw or qp ");
+    hive.insert("n345l7", "__ rx __ __ uv yq __ os qy vu __ tw ");
+    hive.insert("n345", "__ zq __ __ ry wu __ uw yr __ ot qz ");
+    hive.insert("n45l2", "vo __ __ xu __ ww __ ux yt __ ov qq ");
+    hive.insert("n5l2", "vp __ __ pv rq __ __ uy yu __ ow qr ");
+    hive.insert("n67m2", "__ ux __ __ ov qq vo __ ty xu __ ww ");
+    hive.insert("n6m2", "qr vp __ __ pv rq wo __ uy yu __ __ ");
 
-    let mut zz = BTreeMap::new();
-     zz.insert("j2","HgHg PuFe ____ ____ CuNp PbAu ____ AuPb ____ AgUr ____ FePu ");
-     zz.insert("j3","HgSn ____ SnHg UrFe ____ PbAg ____ AuAu ____ AgPb ____ FeUr ");
-     zz.insert("j5","PbCu ____ AuSn ____ AgHg TiFe FeTi ____ ____ SnAu ____ CuPb ");
-     zz.insert("j6","HgAu ____ SnPb ____ CuUr PbSn ____ AuHg NpFe ____ ____ FeNp ");
-     zz.insert("k1","____ FeUr HgSn ____ SnHg UrFe ____ PbAg ____ AuAu ____ AgPb ");
-     zz.insert("k2","NpCu ____ ____ FePu HgHg PuFe ____ UrAg ____ PbAu ____ AuPb ");
-     zz.insert("k5","UrCu ____ PbSn ____ AuHg NpFe ____ ____ FeNp HgAu ____ SnPb ");
-     zz.insert("k6","HgAg ____ SnAu ____ CuPb PbCu ____ AuSn ____ ____ TiFe FeTi ");
-     zz.insert("n0","HgCu ____ SnSn ____ CuHg PbFe ____ AuAg ____ AgAu ____ FePb ");
-    zz.insert("j17","____ ____ SnAu ____ CuPb PbCu ____ AuSn ____ AgHg TiFe FeTi ");
-    zz.insert("j23","HgHg PuFe ____ UrAg ____ PbAu ____ AuPb ____ AgUr ____ FePu ");
-    zz.insert("j25","FeCu HgMn ____ ____ MnHg CuFe PbTi ____ ____ NpAu ____ TiPb ");
-    zz.insert("j26","HgHg PuFe ____ ____ CuNp PbAu ____ AuPb NpCu ____ ____ FePu ");
-    zz.insert("j36","HgAu ____ SnPb UrCu ____ PbSn ____ AuHg NpFe ____ ____ FeNp ");
-    zz.insert("j56","UrCu ____ PbSn ____ AuHg NpFe AgTi ____ FeNp ____ ____ SnPb ");
-    zz.insert("k12","____ AgUr ____ FePu HgHg PuFe ____ UrAg ____ PbAu ____ AuPb ");
-    zz.insert("k15","____ CuUr PbSn ____ AuHg NpFe ____ ____ FeNp HgAu ____ SnPb ");
-    zz.insert("k25","NpCu ____ ____ FePu HgHg PuFe ____ ____ CuNp PbAu ____ AuPb ");
-    zz.insert("k26","HgMn ____ ____ MnHg CuFe PbTi ____ AuNp ____ ____ TiPb FeCu ");
-    zz.insert("k34","PbCu ____ AuSn ____ ____ TiFe FeTi HgAg ____ SnAu ____ CuPb ");
-    zz.insert("k56","HgAu ____ SnPb ____ CuUr PbSn ____ ____ NpFe ____ TiAg FeNp ");
-   zz.insert("j236","HgHg PuFe ____ UrAg ____ PbAu ____ AuPb NpCu ____ ____ FePu ");
-   zz.insert("j256","FeCu HgMn ____ ____ MnHg CuFe PbTi ____ AuNp ____ ____ TiPb ");
-   zz.insert("j2k5","FeCu HgMn ____ ____ MnHg CuFe ____ ____ AuNp NpAu ____ TiPb ");
-   zz.insert("j2k6","HgHg PuFe ____ ____ CuNp PbAu ____ AuPb ____ ____ TiSn FePu ");
-   zz.insert("j2y3","HgHg PuFe SnTi ____ ____ PbAu ____ AuPb ____ AgUr ____ FePu ");
-   zz.insert("j3k5","NpCu ____ TiSn FePu ____ PuFe ____ ____ CuNp PbAu ____ AuPb ");
-   zz.insert("j3k6","HgTi ____ SnNp UrAu ____ PbPb ____ AuUr ____ ____ TiHg FeFe ");
-   zz.insert("j5y6","PbCu ____ AuSn ____ AgHg TiFe FeTi HgAg ____ ____ ____ CuPb ");
-   zz.insert("k125","____ AgUr ____ FePu HgHg PuFe ____ ____ CuNp PbAu ____ AuPb ");
-   zz.insert("k1j5","____ AuUr NpSn ____ TiHg FeFe HgTi ____ ____ UrAu ____ PbPb ");
-   zz.insert("k1j6","____ PuFe SnTi ____ CuNp PbAu ____ AuPb NpCu ____ ____ FePu ");
-   zz.insert("k256","HgMn ____ ____ MnHg CuFe PbTi ____ ____ NpAu ____ TiPb FeCu ");
-   zz.insert("k2j5","NpCu ____ ____ FePu HgHg PuFe SnTi ____ ____ PbAu ____ AuPb ");
-   zz.insert("k2j6","HgMn ____ ____ MnHg CuFe PbTi ____ AuNp NpAu ____ ____ FeCu ");
-   zz.insert("k2x1","____ ____ TiSn FePu HgHg PuFe ____ UrAg ____ PbAu ____ AuPb ");
-   zz.insert("k6x5","HgAg ____ SnAu ____ CuPb PbCu ____ ____ ____ AgHg TiFe FeTi ");
-   zz.insert("n167","NpCu ____ ____ FePu ____ PuFe SnTi ____ CuNp PbAu ____ AuPb ");
-   zz.insert("n345","____ PuFe ____ ____ CuNp PbAu ____ AuPb NpCu ____ TiSn FePu ");
-   zz.insert("n5y2","HgMn ____ ____ MnHg CuFe ____ ____ AuNp NpAu ____ TiPb FeCu ");
-   zz.insert("n6x2","FeCu HgMn ____ ____ MnHg CuFe PbTi ____ AuNp NpAu ____ ____ ");
-  zz.insert("j17k2","____ ____ ____ MnFe CuTi PbAg ____ AuAu ____ AgPb TiCu FeMn ");
-  zz.insert("j17y2","HgAg ____ ____ ____ CuPb PbCu ____ AuSn ____ AgHg TiFe FeTi ");
-  zz.insert("j23k6","HgHg PuFe ____ UrAg ____ PbAu ____ AuPb ____ ____ TiSn FePu ");
-  zz.insert("j25y6","TiCu FeMn ____ ____ SnHg MnFe CuTi PbAg ____ ____ ____ AgPb ");
-  zz.insert("j26y3","HgHg PuFe SnTi ____ ____ PbAu ____ AuPb NpCu ____ ____ FePu ");
-  zz.insert("j2k34","TiCu FeMn ____ ____ ____ MnFe CuTi PbAg ____ AuAu ____ AgPb ");
-  zz.insert("j2k56","HgHg PuFe ____ ____ CuNp PbAu ____ ____ NpCu ____ TiSn FePu ");
-  zz.insert("j34k6","HgSn ____ SnHg MnFe CuTi ____ ____ AuAu ____ ____ TiCu FeMn ");
-  zz.insert("j56y7","UrCu ____ PbSn ____ AuHg NpFe AgTi ____ FeNp HgAu ____ ____ ");
-  zz.insert("k12j5","____ AgUr ____ FePu HgHg PuFe SnTi ____ ____ PbAu ____ AuPb ");
-  zz.insert("k17j5","TiCu FeMn HgSn ____ SnHg MnFe CuTi ____ ____ AuAu ____ ____ ");
-  zz.insert("k25x1","____ ____ TiSn FePu HgHg PuFe ____ ____ CuNp PbAu ____ AuPb ");
-  zz.insert("k26x5","HgSn ____ ____ MnFe CuTi PbAg ____ ____ ____ AgPb TiCu FeMn ");
-  zz.insert("k2j56","NpCu ____ ____ FePu HgHg PuFe SnTi ____ CuNp ____ ____ AuPb ");
-  zz.insert("k34x2","PbCu ____ ____ ____ AgHg TiFe FeTi HgAg ____ SnAu ____ CuPb ");
-  zz.insert("k56x4","HgAu ____ SnPb ____ CuUr ____ ____ AuHg NpFe ____ TiAg FeNp ");
-  zz.insert("n25x6","TiCu FeMn HgSn ____ ____ MnFe CuTi PbAg ____ ____ ____ AgPb ");
-  zz.insert("n26y5","____ ____ SnHg MnFe CuTi PbAg ____ ____ ____ AgPb TiCu FeMn ");
-  zz.insert("n45y2","HgTi ____ ____ UrAu ____ PbPb ____ AuUr NpSn ____ TiHg FeFe ");
-  zz.insert("n67x2","____ AuUr ____ ____ TiHg FeFe HgTi ____ SnNp UrAu ____ PbPb ");
- zz.insert("j136y7","____ ____ SnPb UrCu ____ PbSn ____ AuHg NpFe AgTi ____ FeNp ");
- zz.insert("j167y2","HgAu ____ ____ ____ CuUr PbSn ____ AuHg NpFe ____ TiAg FeNp ");
- zz.insert("j246y3","HgHg PuFe SnTi ____ CuNp ____ ____ AuPb NpCu ____ ____ FePu ");
- zz.insert("j26y34","HgHg PuFe SnTi UrAg ____ ____ ____ AuPb NpCu ____ ____ FePu ");
- zz.insert("j2k6x5","HgHg PuFe ____ ____ CuNp PbAu ____ ____ ____ AgUr TiSn FePu ");
- zz.insert("j2k6y3","HgHg PuFe SnTi ____ ____ PbAu ____ AuPb ____ ____ TiSn FePu ");
- zz.insert("j346y5","NpCu ____ TiSn FePu HgHg PuFe ____ ____ CuNp ____ ____ AuPb ");
- zz.insert("j3k5x4","HgAu ____ SnPb UrCu ____ ____ ____ AuHg NpFe AgTi ____ FeNp ");
- zz.insert("k135x4","____ CuUr PbSn ____ ____ NpFe ____ TiAg FeNp HgAu ____ SnPb ");
- zz.insert("k157x6","HgHg PuFe SnTi ____ CuNp PbAu ____ ____ NpCu ____ ____ FePu ");
- zz.insert("k1j6y7","____ CuUr PbSn ____ AuHg NpFe ____ TiAg FeNp HgAu ____ ____ ");
- zz.insert("k257x1","NpCu ____ TiSn FePu HgHg PuFe ____ ____ CuNp PbAu ____ ____ ");
- zz.insert("k25x17","____ AgUr TiSn FePu HgHg PuFe ____ ____ CuNp PbAu ____ ____ ");
- zz.insert("k2j5x1","____ ____ TiSn FePu HgHg PuFe SnTi ____ ____ PbAu ____ AuPb ");
- zz.insert("k2j5y6","NpCu ____ ____ FePu HgHg PuFe SnTi UrAg ____ ____ ____ AuPb ");
- zz.insert("k345x2","UrCu ____ ____ ____ AuHg NpFe AgTi ____ FeNp HgAu ____ SnPb ");
- zz.insert("n167x4","HgAu ____ ____ UrCu ____ PbSn ____ AuHg NpFe AgTi ____ FeNp ");
- zz.insert("n345y7","____ CuUr ____ ____ AuHg NpFe ____ TiAg FeNp HgAu ____ SnPb ");
-zz.insert("j2k56x4","HgHg PuFe ____ ____ CuNp ____ ____ AuPb NpCu ____ TiSn FePu ");
-zz.insert("j3k56x4","HgTi ____ SnNp UrAu ____ ____ ____ AuUr NpSn ____ TiHg FeFe ");
-zz.insert("k1j56y7","____ AuUr NpSn ____ TiHg FeFe HgTi ____ SnNp UrAu ____ ____ ");
-zz.insert("k2j56y7","NpCu ____ ____ FePu HgHg PuFe SnTi ____ CuNp PbAu ____ ____ ");
-
-    println!("");
-
-    for (k, v) in zz {
+    for (k, v) in hive {
         println!("");
-        println!("\t{}{}", k, " ");
-        println!("\t{}{}", &v[25..60], &v[00..25]); // Fn
-        println!("\t{}{}", &v[00..60], &v[00..00]); // Cn
-        println!("\t{}{}", &v[35..60], &v[00..35]); // Gn
-        println!("\t{}{}", &v[10..60], &v[00..10]); // Dn
-        println!("\t{}{}", &v[45..60], &v[00..45]); // An
-        println!("\t{}{}", &v[20..60], &v[00..20]); // En
-        println!("\t{}{}", &v[55..60], &v[00..55]); // Bn
-        println!("");
+        println!("\t{}{}", k, "-beadgcf");
+        println!("\t{}{}", &v[30..36], &v[00..30]); // Bj
+        println!("\t{}{}", &v[15..36], &v[00..15]); // Fn
+        println!("\t{}{}", &v[00..36], &v[00..00]); // Cn
+        println!("\t{}{}", &v[21..36], &v[00..21]); // Gn
+        println!("\t{}{}", &v[06..36], &v[00..06]); // Dn
+        println!("\t{}{}", &v[27..36], &v[00..27]); // An
+        println!("\t{}{}", &v[12..36], &v[00..12]); // En
+        println!("\t{}{}", &v[33..36], &v[00..33]); // Bn
+        println!("\t{}{}", &v[18..36], &v[00..18]); // Fk
     }
 
     println!("");
-
 }
-
